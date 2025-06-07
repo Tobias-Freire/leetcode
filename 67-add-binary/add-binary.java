1190cb822ce84ecb817675e3c7c56682cd5810f9
Tobias Freire Numeriano <150594599+Tobias-Freire@users.noreply.github.com>
2025-06-07 08:27:51 -0300

    Time: 2 ms (41.87%) | Memory: 42.2 MB (66.18%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+
+        StringBuilder result = new StringBuilder();
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0) {
+                carry += a.charAt(i) - '0';
+                i--; 
+            }
+
+            if (j >= 0) {
+                carry += b.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(carry % 2);
+            carry /= 2;
+        }
+
+        return result.reverse().toString();
+    }
+}
